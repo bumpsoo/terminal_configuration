8855dd33e0c7c5e159a001e085429ac30fc7387a
bumpsoo <bumpsoo063@protonmail.ch>
2024-02-19 20:37:57 +0900

    added installation script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+# go
+if [ -z "$GO_VERSION" ] then
+  GO_VERSION = "1.22.0"
+fi
+
+curl -LO https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+
