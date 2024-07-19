96bd0e0e1c7261d568efc10393be90005df0cd1a
bumpsoo <bumpsoo063@protonmail.ch>
2024-07-19 23:55:06 +0900

    fix: make telescope search hidden files

--- .config/nvim/lua/bumpsoo/plugins/telescope.lua
+++ .config/nvim/lua/bumpsoo/plugins/telescope.lua
@@ -5,6 +5,22 @@ return {
     'BurntSushi/ripgrep'
   },
   config = function()
+    local telescope = require("telescope")
+    local telescopeConfig = require("telescope.config")
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+    table.insert(vimgrep_arguments, "--hidden")
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
+    })
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
