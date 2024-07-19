df6ba5dd9ba47d7d872c7368d1485a8336fefd3c
bumpsoo <bumpsoo063@protonmail.ch>
2024-07-19 10:12:57 +0900

    feat: telescope_browser added

--- .config/nvim/lua/bumpsoo/plugins/telescope.lua
+++ .config/nvim/lua/bumpsoo/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.4',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'BurntSushi/ripgrep'
+  },
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
